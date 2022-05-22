e7cd799cb5fe4c84eebf043a122a2e30879c37e7
Richard Aiello <richaiello17@gmail.com>
2022-05-22 11:11:34 -0400

    Added comments to index.js.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
+// Hamburger button variables
 const hamburgerBtn = document.querySelector('.btn--hamburger');
 const hamburgerTop = document.querySelector('.hamburger--top');
 const hamburgerBottom = document.querySelector('.hamburger--bottom');
 
-const navBtns = [...document.querySelectorAll('.btn--nav')];
-const navLists = [...document.querySelectorAll('.nav__list--secondary')];
-const navArrows = [...document.querySelectorAll('.arrow')];
+// Nav variables
 
+// Immutably spreads the NodeList to an array.
+const navBtns = [...document.querySelectorAll('.btn--nav')];
+// Immutably spreads the NodeList to an array.
+const navLists = [...document.querySelectorAll('.nav__list--secondary')];
+// Immutably spreads the NodeList to an array.
+const navArrows = [...document.querySelectorAll('.arrow')];
 const navMenu = document.querySelector('.nav__menu');
 const navShadow = document.querySelector('.nav__shadow');
 
+/* Disables the hamburger button to prevent button spamming and rotates the hamburger 
+icon into an "X". Adds the hidden and animation classes to navMenu and navShadow. Uses 
+an asynchronous function to re-enable the hamburger btn and removes animation
+classes from navMenu and navShadow upon animation completion. The setTimeout time limit 
+and animation time are both 500ms. */
 const navShow = () => {
 
   setTimeout(() => {
@@ -30,6 +40,11 @@ const navShow = () => {
   navShadow.classList.remove('hidden');
 }
 
+/* Disables the hamburger button to prevent button spamming and reverts the hamburger
+icon back to it's original shape. Adds the hidden and animation classes to navMenu and 
+navShadow. Uses an asynchronous function to re-enable the hamburger btn and removes animation
+classes from navMenu and navShadow upon animation completion. The setTimeout time limit 
+and animation time are both 500ms. */
 const navHide = () => {
 
   setTimeout(() => {
@@ -51,11 +66,13 @@ const navHide = () => {
   navShadow.classList.add('fade-out');
 }
 
+// Removes hidden class from nav__list--secondary and rotates its arrow.
 const listShow = (list, arrow) => {
   list.classList.remove('hidden');
   arrow.classList.add('rotate');
 }
 
+// Adds hidden class to nav__list--secondary and rotates its arrow.
 const listHide = (list, arrow) => {
   list.classList.add('hidden');
   arrow.classList.remove('rotate');
@@ -65,12 +82,18 @@ const listHide = (list, arrow) => {
   EVENT LISTENERS
 **************************************/
 
+/* Uses the ternary operator to determine whether to show or hide the nav overlay,
+based on the existence of class hidden in navMenu. */
 hamburgerBtn.addEventListener('click', e => {
   navMenu.classList.contains('hidden') ?
   navShow() :
   navHide();
 });
 
+/* Uses the ternary operator to determine whether to show or hide the corresponding
+nav__list--secondary, based on the existence of class hidden in the current list. 
+Uses the index of the button that is being clicked to run a function on the proper
+list. */
 navBtns.forEach(btn => {
   btn.addEventListener('click', e => {
     const currentList = navLists[navBtns.indexOf(e.currentTarget)];
